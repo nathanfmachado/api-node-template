61b4fc87950b99562acf79e8a4b24981377bf8c4
Nathan Fabiano Machado <nathan.machado@taqtile.com.br>
2023-12-03 22:02:04 -0300

    Configure basic ENV variables

---
+++ src/env/index.ts
@@ -0,0 +1,18 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'test', 'stg', 'prod']).default('dev'),
+  PORT: z.coerce.number().default(4000),
+  DATABASE_URL: z.string(),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (!_env.success) {
+  console.log('Invalid environment variables:', _env.error.format());
+  throw new Error('Invalid environment variables');
+}
+
+export const env = _env.data;
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import { app } from "./app";
+import { env } from "./env";
 
 app.listen({
   host: '0.0.0.0',
-  port: 4000,
+  port: env.PORT,
 }).then(() => {
   console.log(`HTTP server listening at http://localhost:4000`);
 })
