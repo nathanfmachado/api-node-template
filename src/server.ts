f794158a6b99ddff9a181d4ca5830bc60a16f385
Nathan Fabiano Machado <nathan.machado@taqtile.com.br>
2023-12-03 22:22:09 -0300

    Configure eslint and ts import paths

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import { app } from "./app";
-import { env } from "./env";
+import { app } from './app';
+import { env } from './env';
 
 app.listen({
   host: '0.0.0.0',
   port: env.PORT,
 }).then(() => {
-  console.log(`HTTP server listening at http://localhost:4000`);
-})
+  console.log('HTTP server listening at http://localhost:4000');
+});
